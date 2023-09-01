0524bd9aee4b2d6deb089e4c60d58da5331e48d7
nahidraj <nahidraj.web@gmail.com>
2023-09-01 20:59:31 +0600

    change arrow font size

--- dashboard-layout/sidebar.php
+++ dashboard-layout/sidebar.php
@@ -5,7 +5,7 @@
       <li class="active"><a href="#">Dashboard</a></li>
       <li><a href="#">Order</a></li>
       <li class="sub-menu">
-        <a href="#">Product <i class="fa-solid fa-angle-right float-end"></i></a>
+        <a>Product <i class="fa-solid fa-angle-right float-end"></i></a>
         <ul style="display: none;" class="list-unstyled ps-3">
           <li><a href="">Aims & Objectives</a></li>
           <li><a href="">BCS Historical</a></li>
@@ -21,7 +21,14 @@
       <li><a href="#">Layout</a></li>
       <li><a href="#">Team</a></li>
       <li><a href="#">Payment</a></li>
-      <li><a href="#">Career</a></li>
+      <li class="sub-menu">
+        <a>Career <i class="fa-solid fa-angle-right float-end"></i></a>
+        <ul style="display: none;" class="list-unstyled ps-3">
+          <li><a href="">Aims & Objectives</a></li>
+          <li><a href="">BCS Historical</a></li>
+          <li><a href="">Activities</a></li>
+        </ul>
+      </li>
       <li><a href="#">Setting</a></li>
     </ul>
 
