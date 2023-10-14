935c1af98c9049122af83a60a35d3cc860d74941
nahid12 <nahidraj.web@gmail.com>
2023-10-14 20:34:17 +0600

    customer registration and login page

---
+++ customer-login.php
@@ -0,0 +1,52 @@
+<?php
+include("website-layout/header.php");
+?>
+
+
+<!-- customer registration panel start -->
+<section class="customer_login">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 mx-auto">
+        <div class="title">
+          <h3>Marchent Account Login!</h3>
+          <h5>(Secure Your Account)</h5>
+        </div>
+        <div class="form_main">
+          <div class="form_box">
+            <form action="#">
+              <div class="user_icon">
+                <i class="fa-solid fa-user"></i>
+              </div>
+              <div class="input_groups">
+                <input type="text" placeholder="Username">
+              </div>
+              <div class="input_groups">
+                <input type="text" placeholder="Password">
+              </div>
+
+              <div class="button">
+                <button type="submit">Login
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="fotter_part d-flex justify-content-between">
+            <div class="btn_item">
+              <a href="" class="action_btn create">Create Account</a>
+            </div>
+            <div class="btn_item">
+              <a href="" class="action_btn forgot">Forget Passwod</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- customer registration panel end -->
+
+
+<?php
+include("website-layout/footer.php");
+?>
---
+++ customer-registration.php
@@ -0,0 +1,57 @@
+<?php
+include("website-layout/header.php");
+?>
+
+
+<!-- customer registration panel start -->
+<section class="customer_registration">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <div class="title">
+          <h3>Welcome to Tools Bangladesh</h3>
+          <h3>Customer Registration!</h3>
+        </div>
+        <form action="#">
+          <div class="form_main">
+            <div class="user_icon">
+              <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input_groups d-flex align-items-center">
+              <label>Full Name: </label>
+              <span>*</span>
+              <input type="text">
+            </div>
+            <div class="input_groups d-flex align-items-center">
+              <label>Email Address: </label>
+              <span>*</span>
+              <input type="text">
+            </div>
+            <div class="input_groups d-flex align-items-center">
+              <label>Phone Number: </label>
+              <span>*</span>
+              <input type="text">
+            </div>
+            <div class="form-check d-flex align-items-center">
+              <input class="form-check-input shadow-none" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Are agree with the terms & conditions!
+              </label>
+            </div>
+
+            <div class="button">
+              <button type="submit">Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- customer registration panel end -->
+
+
+<?php
+include("website-layout/footer.php");
+?>
